7b790a11808d82c2cf6bbef2b3d8a7017c9ebdb6
Andrea <a.g.baldan@gmail.com>
2023-12-09 22:05:25 +0000

    Small refactor of the refreshing utility functions in the client

--- chatlite_client.c
+++ chatlite_client.c
@@ -22,6 +22,7 @@
  *
  */
 
+#include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <netdb.h>
@@ -154,80 +155,21 @@ void tty_raw_mode_disable_atexit(void) { tty_raw_mode_disable(STDIN_FILENO); }
 
 void pty_clear_current_line(void) { write(STDOUT_FILENO, "\e[2K", 4); }
 
-void pty_cursor_at_line_start(void) { write(STDOUT_FILENO, "\r", 1); }
-
-int pty_get_window_size(int *rows, int *cols) {
-    struct winsize ws;
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
-        return -1;
-
-    *cols = ws.ws_col;
-    *rows = ws.ws_row;
-    return 0;
+void pty_cursor_at_line_start(void) {
+    write(STDOUT_FILENO, "\r\x1b[1m>\x1b[0m ", 11);
 }
 
-// Reset position to the bottom left
-int pty_reset_cursor_position(void) {
-    int r, c;
-    int err = pty_get_window_size(&r, &c);
-    if (err < 0)
-        return err;
-
-    char esc[16];
-    snprintf(esc, sizeof(esc), "\x1b[%d;0H", r);
-    return write(STDOUT_FILENO, esc, strlen(esc));
-}
-
-int pty_draw_status_bar(void) {
-    int err, r, c;
-    err = pty_get_window_size(&r, &c);
-    if (err < 0)
-        return err;
-    // This switches to inverted colors.
-    // NOTE:
-    // The m command (Select Graphic Rendition) causes the text printed
-    // after it to be printed with various possible attributes including
-    // bold (1), underscore (4), blink (5), and inverted colors (7). An
-    // argument of 0 clears all attributes (the default one). See
-    // http://vt100.net/docs/vt100-ug/chapter3.html#SGR for more info.
-    char buf[2048];
-    int n = snprintf(buf, sizeof(buf), "\x1b[0;0H\x1b[7m");
-    // 7 is just the dumb half of the header `chatlite client`
-    int mid = (c / 2) - 7;
-    int i = 0, offset = 0;
-    while (i < mid) {
-        buf[n + i] = ' ';
-        i++;
-    }
-    offset = n + mid;
-    strncpy(buf + offset, "chatlite client", 16);
-    i = 0;
-    // 15 is just strlen("chatlite client")
-    offset += 15;
-    while (i < mid) {
-        buf[offset + i] = ' ';
-        i++;
-    }
-    offset += mid;
-    strncpy(buf + offset, "\x1b[m\r\n", 6);
-    return write(STDOUT_FILENO, buf, strlen(buf));
-}
-
-void pty_clear_screen(void) { write(STDOUT_FILENO, "\x1b[2J", 4); }
-void pty_save_cursor_position(void) { write(STDOUT_FILENO, "\x1b[s", 3); }
-void pty_unsave_cursor_position(void) { write(STDOUT_FILENO, "\x1b[u", 3); }
-
-#define BUFSIZE 128
+#define BUFSIZE 1024
 #define IB_OK 0
 #define IB_ERR -1
 #define IB_NEWLINE 2
 
-struct input_buffer {
+struct buffer {
     char buf[BUFSIZE];
     int len;
 };
 
-int input_buffer_append(struct input_buffer *ib, char c) {
+int buffer_append(struct buffer *ib, char c) {
     if (ib->len >= BUFSIZE)
         return IB_ERR;
 
@@ -235,8 +177,8 @@ int input_buffer_append(struct input_buffer *ib, char c) {
     return IB_OK;
 }
 
-void input_buffer_hide(const struct input_buffer *ib);
-void input_buffer_show(const struct input_buffer *ib);
+void buffer_hide(const struct buffer *ib);
+void buffer_show(const struct buffer *ib);
 
 /*
  * Process every new keystroke arriving from the keyboard. As a side effect
@@ -244,7 +186,7 @@ void input_buffer_show(const struct input_buffer *ib);
  * the user is typing, so that reading the input buffer 'buf' for 'len'
  * bytes will contain it.
  */
-int input_buffer_feed_char(struct input_buffer *ib, int c) {
+int buffer_feed_char(struct buffer *ib, int c) {
     switch (c) {
     case '\n':
         break; // Ignored. We handle \r instead.
@@ -253,46 +195,59 @@ int input_buffer_feed_char(struct input_buffer *ib, int c) {
     case 127: // Backspace.
         if (ib->len > 0) {
             ib->len--;
-            input_buffer_hide(ib);
-            input_buffer_show(ib);
+            buffer_hide(ib);
+            buffer_show(ib);
         }
         break;
     default:
-        if (input_buffer_append(ib, c) == 0)
+        if (buffer_append(ib, c) == 0)
             write(STDOUT_FILENO, ib->buf + ib->len - 1, 1);
         break;
     }
     return IB_OK;
 }
 
+int buffer_append_string(struct buffer *b, const char *src, size_t len) {
+    assert(b->len + len <= BUFSIZE);
+    memcpy(b->buf, src, len);
+    b->len += len;
+    return 0;
+}
+
 // Hide the line the user is typing
-void input_buffer_hide(const struct input_buffer *ib) {
+void buffer_hide(const struct buffer *ib) {
     (void)ib; // Not used var, but is conceptually part of the API.
     pty_clear_current_line();
     pty_cursor_at_line_start();
 }
 
 // Show again the current line
-void input_buffer_show(const struct input_buffer *ib) {
-    write(fileno(stdout), ib->buf, ib->len);
+void buffer_show(const struct buffer *ib) {
+    write(STDOUT_FILENO, ib->buf, ib->len);
+    fflush(stdout);
 }
 
 // Reset the buffer to be empty
-void input_buffer_clear(struct input_buffer *ib) {
+void buffer_clear(struct buffer *ib) {
     memset(ib->buf, 0x00, ib->len);
     ib->len = 0;
-    input_buffer_hide(ib);
+    buffer_hide(ib);
 }
 
-size_t input_buffer_fmt(const struct input_buffer *ib, char *dst) {
+void buffer_dump(struct buffer *b) {
+    buffer_show(b);
+    buffer_clear(b);
+}
+
+size_t buffer_fmt(const struct buffer *ib, char *dst) {
     char ts_str[64] = {0};
     time_t t = time(NULL);
     struct tm *tmp = localtime(&t);
     size_t ts_len = strftime(ts_str, sizeof(ts_str), "%T", tmp);
-    // 9 is the number of additional format chars we're adding + 1 for the nul
+    // 15 is the number of additional format chars we're adding + 1 for the nul
     // character
-    size_t maxlen = strlen(ib->buf) + ts_len + 9;
-    return snprintf(dst, maxlen, "[%s you]: %s", ts_str, ib->buf);
+    size_t maxlen = strlen(ib->buf) + ts_len + 16;
+    return snprintf(dst, maxlen, "\x1b[1m[%s you]:\x1b[m %s", ts_str, ib->buf);
 }
 
 /*
@@ -337,30 +292,119 @@ size_t message_fmt(const struct message *m, char *buf) {
     time_t t = time(NULL);
     struct tm *tmp = localtime(&t);
     size_t ts_len = strftime(ts_str, sizeof(ts_str), "%T", tmp);
-    // 7 is the number of additional format chars we're adding + 1 for the nul
+    // 13 is the number of additional format chars we're adding + 1 for the nul
     // character
-    size_t maxlen = strlen(m->nick) + strlen(m->content) + ts_len + 7;
-    return snprintf(buf, maxlen, "[%s %s]: %s\n", ts_str, m->nick, m->content);
+    size_t maxlen = strlen(m->nick) + strlen(m->content) + ts_len + 14;
+    return snprintf(buf, maxlen, "\x1b[1m[%s %s]:\x1b[m %s\n", ts_str, m->nick,
+                    m->content);
 }
+
+int pty_get_window_size(int *rows, int *cols) {
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+        return -1;
+
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+}
+
+// Reset position to the bottom left
+int pty_reset_cursor_position(struct buffer *b) {
+    int r, c;
+    int err = pty_get_window_size(&r, &c);
+    if (err < 0)
+        return err;
+
+    char esc[16];
+    int len = snprintf(esc, sizeof(esc), "\x1b[%d;0H >> ", r);
+    return buffer_append_string(b, esc, len);
+}
+
+int pty_draw_status_bar(struct buffer *b) {
+    int err, r, c;
+    err = pty_get_window_size(&r, &c);
+    if (err < 0)
+        return err;
+    // This switches to inverted colors.
+    // NOTE:
+    // The m command (Select Graphic Rendition) causes the text printed
+    // after it to be printed with various possible attributes including
+    // bold (1), underscore (4), blink (5), and inverted colors (7). An
+    // argument of 0 clears all attributes (the default one). See
+    // http://vt100.net/docs/vt100-ug/chapter3.html#SGR for more info.
+    char buf[2048];
+    int n = snprintf(buf, sizeof(buf), "\x1b[0;0H\x1b[7m");
+    // 7 is just the dumb half of the header `chatlite client`
+    int mid = (c / 2) - 7;
+    int i = 0, offset = 0;
+    while (i < mid) {
+        buf[n + i] = ' ';
+        i++;
+    }
+    offset = n + mid;
+    memcpy(buf + offset, "\x1b[1mchatlite client", 19);
+    i = 0;
+    // 19 is just strlen("\x1b[1mchatlite client")
+    offset += 19;
+    while (i < mid) {
+        buf[offset + i] = ' ';
+        i++;
+    }
+    offset += mid;
+    memcpy(buf + offset, "\x1b[m\r\n", 5);
+    int len = strlen(buf);
+    return buffer_append_string(b, buf, len);
+}
+
+int pty_refresh(struct buffer *b) {
+    buffer_clear(b);
+    int err = pty_draw_status_bar(b);
+    if (err < 0)
+        return err;
+    buffer_dump(b);
+    err = pty_reset_cursor_position(b);
+    if (err < 0)
+        return err;
+    buffer_dump(b);
+    /* buffer_show(b); */
+    return 0;
+}
+
+int pty_print_message(const struct message *m) {
+    char output[BUFSIZE] = {0};
+    pty_clear_current_line();
+    write(STDOUT_FILENO, "\r", 1);
+    int n = message_fmt(m, output);
+    return write(STDOUT_FILENO, output, n);
+}
+
+int pty_print_buffer(const struct buffer *b) {
+    char output[BUFSIZE] = {0};
+    pty_clear_current_line();
+    write(STDOUT_FILENO, "\r", 1);
+    int n = buffer_fmt(b, output);
+    return write(STDOUT_FILENO, output, n);
+}
+
+void pty_clear_screen(void) { write(STDOUT_FILENO, "\x1b[2J", 4); }
 
 int main(void) {
     int err = tty_raw_mode_enable(STDIN_FILENO);
     if (err < 0)
         exit(EXIT_FAILURE);
 
+    struct buffer ib;
+    buffer_clear(&ib);
+
     pty_clear_screen();
 
-    pty_draw_status_bar();
-    err = pty_reset_cursor_position();
-    if (err < 0)
-        exit(EXIT_FAILURE);
+    pty_refresh(&ib);
 
     int s = socket_connection("localhost", 6699);
     if (s < 0)
         exit(EXIT_FAILURE);
 
-    struct input_buffer ib;
-    input_buffer_clear(&ib);
     fd_set readfds;
 
     while (1) {
@@ -377,7 +421,7 @@ int main(void) {
             exit(EXIT_FAILURE);
         }
 
-        char buf[128];
+        char buf[BUFSIZE];
         memset(buf, 0x00, sizeof(buf));
 
         if (FD_ISSET(s, &readfds)) {
@@ -389,12 +433,8 @@ int main(void) {
             }
             struct message m;
             message_parse(buf, &m, count);
-            char output[128] = {0};
-            int n = message_fmt(&m, output);
-            write(STDOUT_FILENO, output, n);
-
-            pty_draw_status_bar();
-            (void)pty_reset_cursor_position();
+            pty_print_message(&m);
+            pty_refresh(&ib);
         } else if (FD_ISSET(STDIN_FILENO, &readfds)) {
             // Data from the user typing on the terminal
             ssize_t count = read(STDIN_FILENO, buf, sizeof(buf));
@@ -408,22 +448,16 @@ int main(void) {
                     }
                 }
 
-                int res = input_buffer_feed_char(&ib, buf[j]);
+                int res = buffer_feed_char(&ib, buf[j]);
                 switch (res) {
                 case IB_NEWLINE:
-                    input_buffer_append(&ib, '\n');
-                    input_buffer_hide(&ib);
-
-                    char output[128] = {0};
-                    int n = input_buffer_fmt(&ib, output);
-
-                    write(STDOUT_FILENO, output, n);
-
-                    write(s, ib.buf, ib.len);
-                    input_buffer_clear(&ib);
-                    pty_draw_status_bar();
-                    (void)pty_reset_cursor_position();
-
+                    buffer_append(&ib, '\n');
+                    buffer_hide(&ib);
+                    pty_print_buffer(&ib);
+                    // TODO print diagnostic on the term
+                    (void)write(s, ib.buf, ib.len);
+                    buffer_clear(&ib);
+                    pty_refresh(&ib);
                     break;
                 case IB_OK:
                     break;
